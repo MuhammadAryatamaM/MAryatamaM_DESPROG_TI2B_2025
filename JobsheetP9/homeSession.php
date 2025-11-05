e534bdc5ee27b8457dfa718a808e15b1656636f5
MuhammadAryatamaM <muhammad.aryatama2006@gmail.com>
2025-11-05 20:36:47 +0700

    upload jobsheet 9

---
+++ JobsheetP9/homeSession.php
@@ -0,0 +1,24 @@
+<?php
+session_start();
+?>
+<html>
+
+<head>
+</head>
+
+<body>
+  <?php
+  if ($_SESSION['status'] == 'login') {
+    echo "selamat datang " . $_SESSION['username'];
+  ?>
+    <br> <a href="sessionLogout.php"> Log Out </a>
+  <?php
+  } else {
+    echo "anda belum login. silahkan " ?>
+    <a href="sessionLoginForm.html"> Log In</a>
+  <?php
+  }
+  ?>
+</body>
+
+</html>
