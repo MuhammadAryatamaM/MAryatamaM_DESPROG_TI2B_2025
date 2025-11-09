e58c487bcd9c99ddd62fe9e9fb4a81988cb829a8
MuhammadAryatamaM <muhammad.aryatama2006@gmail.com>
2025-11-09 13:37:51 +0700

    add gitignore

--- employee-management/views/footer.php
+++
@@ -1,8 +0,0 @@
-</main>
-
-        <footer class="footer">
-            <p>&copy; <?php echo date('Y'); ?> Sistem Manajemen Karyawan. All rights reserved.</p>
-        </footer>
-    </div>
-</body>
-</html>
