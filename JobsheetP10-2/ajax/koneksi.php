6e2bad3d19a4775bc77d2bd3a8dba95b5d35dc19
MuhammadAryatamaM <muhammad.aryatama2006@gmail.com>
2025-11-09 13:20:25 +0700

    upload jobsheet 10

---
+++ JobsheetP10-2/ajax/koneksi.php
@@ -0,0 +1,8 @@
+<?php
+define('HOST', 'db');
+define('USER', 'rya');
+define('PASS', 'itzarya');
+define('DB1', 'prakwebdb');
+
+// Buat Koneksinya
+$db1 = new mysqli(HOST, USER, PASS, DB1);
---
+++ employee-management/views/footer.php
@@ -0,0 +1,8 @@
+</main>
+
+        <footer class="footer">
+            <p>&copy; <?php echo date('Y'); ?> Sistem Manajemen Karyawan. All rights reserved.</p>
+        </footer>
+    </div>
+</body>
+</html>
